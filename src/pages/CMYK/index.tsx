caa177382f0089300be83d0c06631ff35461e032
LAB-80 <samuellucasvieira.melo@gmail.com>
2024-04-22 22:27:57 -0300

    Atividade 2 desenvolvimento web 2

---
+++ src/pages/CMYK/index.tsx
@@ -0,0 +1,11 @@
+import CMYKColor from "../../components/CMYKColor";
+import { CMYKProvider } from "../../contexts/CMYKContext";
+
+
+export default function CMYK() {
+  return (
+    <CMYKProvider>
+        <CMYKColor />
+    </CMYKProvider>
+  );
+}
